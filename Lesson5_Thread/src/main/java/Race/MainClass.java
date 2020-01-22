3dea8ab46e06409fc9ce67d65ef73681cb31be0d
ElenaKuropatkina <el-vita81@yandex.ru>
2020-01-22 09:53:32 +0300

    Lesson 5.Thread_Synchronizers

---
+++ Lesson5_Thread/src/main/java/Race/MainClass.java
@@ -0,0 +1,35 @@
+package Race;
+
+import java.util.concurrent.CountDownLatch;
+
+public class MainClass {
+
+    public static final int CARS_COUNT = 4;
+    public static final CountDownLatch START = new CountDownLatch(CARS_COUNT + 1);
+    public static final CountDownLatch FINISH = new CountDownLatch(CARS_COUNT);
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+                new Thread(cars[i]).start();
+        }
+
+        while (START.getCount() > 1);
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        START.countDown();
+
+        while (FINISH.getCount() > 0);
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
+
+
+
+
+
+
